177f2b6d923b50c83415ebf4a45d98ad2a885571
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-26 13:24:09 +0100

    hasPlayerWonTheRound function

--- Rock-paper-Scissors/script.js
+++ Rock-paper-Scissors/script.js
@@ -1,3 +1,4 @@
+
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
     const randomIndex = Math.floor(Math.random() * options.length);
@@ -5,3 +6,17 @@ function getRandomComputerResult() {
     return options[randomIndex];
   }
   console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player,computer) {
+    if(player === "Rock" && computer === "Scissors"){
+        return true;
+      } else if(player === "Scissors" && computer === "Paper"){
+        return true;
+      } else if(player === "Paper" && computer === "Rock"){
+        return true;
+      } else {
+        return false;
+      };
+    };
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("scissors", "Rock"));
