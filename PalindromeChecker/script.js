2db1c9a6c7aaac6f4867e6208b34cf463e6c7732
thiagogbarbosa <tthiagogb@gmail.com>
2024-07-29 09:49:31 +0100

    Html setup for the project

---
+++ PalindromeChecker/script.js
@@ -0,0 +1,8 @@
+/* Palindrome Checker. palindrome is a word or phrase that can be read
+the same way forwards and backwards, ignoring punctuation, case and spacing
+
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+and turn everything into the same case (lower or upper case)
+in order to check for palindromes.
+*/
+
