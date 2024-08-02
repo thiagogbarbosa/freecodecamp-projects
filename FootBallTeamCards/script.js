d36b276972036b6077922ecfbeb413f0256a886b
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-02 09:52:31 +0100

    setPlayerCards function and textContent

--- FootBallTeamCards/script.js
+++ FootBallTeamCards/script.js
@@ -175,3 +175,25 @@ Object.freeze(myFavoriteFootballTeam);
 //Object destructuring to access elements
 const {sport, team, year, players} = myFavoriteFootballTeam;
 const {coachName} = myFavoriteFootballTeam.headCoach;
+
+typeOfSport.textContent = sport;
+teamName.textContent = team;
+worldCupYear.textContent = year;
+headCoach.textContent = coachName;
+
+//players as default value. destructuring at ln176
+const setPlayerCards = (arr = players) => {
+  playerCards.innerHTML += arr
+    .map(
+      ({ name, position, number, isCaptain, nickname }) =>
+        `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: ${nickname !== null ? nickname : "N/A"}</p>
+        </div>
+      `
+    )
+    .join(""); //bcs map() will create a new array separated by commas
+};
