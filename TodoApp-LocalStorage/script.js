d18c15af4667793dc8ecb584c15c2cff386fa0ef
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-03 11:22:20 +0100

    configuring buttons and showing modal

--- TodoApp-LocalStorage/script.js
+++ TodoApp-LocalStorage/script.js
@@ -9,3 +9,24 @@ const tasksContainer = document.getElementById("tasks-container");
 const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
+
+//Store all the tasks along with their associated data
+const taskData = []; //And save them on localStorage
+let currentTask = {}; //to track the state when editin and discarding tasks
+
+openTaskFormBtn.addEventListener("click",()=>{
+    taskForm.classList.toggle("hidden")
+  });
+
+closeTaskFormBtn.addEventListener("click",()=>{
+    confirmCloseDialog.showModal();
+});
+
+cancelBtn.addEventListener("click",()=>{
+    confirmCloseDialog.close();
+});
+
+discardBtn.addEventListener("click",()=>{
+    confirmCloseDialog.close();
+    taskForm.classList.toggle("hidden");
+  });
