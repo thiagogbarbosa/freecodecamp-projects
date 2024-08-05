d16379fd79838fc215b897a87230527fbe7017f8
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-05 12:05:50 +0100

    spliting taskForm event in two different functions for code readability

--- TodoApp-LocalStorage/script.js
+++ TodoApp-LocalStorage/script.js
@@ -14,6 +14,39 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = []; //And save them on localStorage
 let currentTask = {}; //to track the state when editin and discarding tasks
 
+const addOrUpdateTask = ()=>{
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+      };
+    
+       if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+      };
+    updateTaskContainer();
+    reset();
+};
+
+const updateTaskContainer = ()=>{
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            (tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+              <p><strong>Title:</strong> ${title}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Description:</strong> ${description}</p>
+              <button type="button" class="btn">Edit</button>
+              <button type="button" class="btn">Delete</button>
+            </div>
+          `)
+        }
+      );    
+
+};
+
 const reset = ()=>{
     titleInput.value = "";
     dateInput.value = "";
@@ -49,35 +82,7 @@ discardBtn.addEventListener("click",()=>{
 taskForm.addEventListener("submit",(e)=>{
     e.preventDefault(); //to stop the page to refresh after submiting
 
-//Determine wheter the task being added to the taskData array already exists
-//if doesn't- add it. If it does- update it.
-
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-      };
-    
-       if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-      }
-    
-      taskData.forEach(({id, title, date, description}) => {
-          (tasksContainer.innerHTML += `
-            <div class="task" id="${id}">
-              <p><strong>Title:</strong> ${title}</p>
-              <p><strong>Date:</strong> ${date}</p>
-              <p><strong>Description:</strong> ${description}</p>
-              <button type="button" class="btn">Edit</button>
-              <button type="button" class="btn">Delete</button>
-            </div>
-          `)
-        } 
-      );
-    
-      reset();
+    addOrUpdateTask();
 });
 
 
