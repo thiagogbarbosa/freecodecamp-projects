d78fb209e7478bb7184e74081a18f20b07242850
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-05 11:40:49 +0100

    taskobj finished

--- TodoApp-LocalStorage/script.js
+++ TodoApp-LocalStorage/script.js
@@ -39,9 +39,30 @@ taskForm.addEventListener("submit",(e)=>{
 
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
     const taskObj = {
-
-    };
-    console.log(taskObj);
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+      };
+    
+       if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+      }
+    
+      taskData.forEach(({id, title, date, description}) => {
+          (tasksContainer.innerHTML += `
+            <div class="task" id="${id}">
+              <p><strong>Title:</strong> ${title}</p>
+              <p><strong>Date:</strong> ${date}</p>
+              <p><strong>Description:</strong> ${description}</p>
+              <button type="button" class="btn">Edit</button>
+              <button type="button" class="btn">Delete</button>
+            </div>
+          `)
+        } 
+      );
+    
+      taskForm.classList.toggle("hidden");
 });
 
 
