72e37d659f6e5eaa3298a38406ae53fbbf0fabe7
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-08 10:38:30 +0100

    decimalToBinary first function

--- DecimalToBinary/script.js
+++ DecimalToBinary/script.js
@@ -2,7 +2,29 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
+const decimalToBinary = (input) => {
+  const inputs = [];
+  const quotients = [];
+  const remainders = [];
 
+
+
+  while (input > 0) {
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
+  }
+
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
+
+  result.innerText = remainders.reverse().join("");
+};
 
 const checkUserInput = () => {
   if (
@@ -14,7 +36,8 @@ const checkUserInput = () => {
     return;
   }
 
-  console.log(numberInput.value);
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
 };
 
 convertBtn.addEventListener("click", checkUserInput);
