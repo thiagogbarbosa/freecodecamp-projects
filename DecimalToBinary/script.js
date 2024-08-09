1010be2a009316791f8d6ecec46583f840a25999
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-09 10:36:18 +0100

    Changing for recursive function

--- DecimalToBinary/script.js
+++ DecimalToBinary/script.js
@@ -5,18 +5,13 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "0";
+  }else if(input === 1){
+    return "1"
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -29,7 +24,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
