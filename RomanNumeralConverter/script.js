5cc8e212f18fb7cc1ed924be0f71a9689085efa0
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-12 10:59:51 +0100

    fixing DOM error and inputHandler function

--- RomanNumeralConverter/script.js
+++ RomanNumeralConverter/script.js
@@ -1,8 +1,8 @@
-const numberInput = document.getElementById("input");
+const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-const romanNumberals = {
+const romanNumerals = {
     M: 1000,
     CM:900,
     D: 500,
@@ -18,29 +18,39 @@ const romanNumberals = {
     I: 1,
 };
 
-/*
+
 const inputHandler = (input)=>{
+    if(!input || input === null){
+      output.textContent = "Please enter a valid number"  
+    } else if(input<0){
+        output.textContent = "Please enter a number greater than or equal to 1"
+    } else if(input>=4000){
+        output.textContent = "Please enter a number less or equal to 3999"
+    } else{
+        output.textContent = arabToRoman(input);
+    }
     //se for nulo: Please enter a valid number
     //se for negativo: please enter a number greater than or equal to 1
     //se for maior que 3999: enter a number less or equal to 3999
 
-}*/
+}
 
 const arabToRoman = (userInput)=>{
     let romanOutput = "";
     let input = userInput;
-    const objKeys= Object.keys(romanNumberals);
-    const objValues = Object.values(romanNumberals);
+    const objKeys= Object.keys(romanNumerals);
+    const objValues = Object.values(romanNumerals);
     for(let i = 0; objValues[i]>=1; i++){
         while(input>=objValues[i]){
             romanOutput += objKeys[i]; 
             input -= objValues[i];
         };
     };
-
-    console.log(romanOutput);
-
+    return romanOutput;
 }; 
 
-arabToRoman(3290);
-
+convertBtn.addEventListener("click",()=>{
+    const inputValue = parseInt(numberInput.value);
+    console.log("testando aqui",inputValue);
+    inputHandler(inputValue);
+});
