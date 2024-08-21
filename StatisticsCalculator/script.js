f47e763c11c9466bccb9268009e2416f7c4d6429
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-21 19:11:40 +0100

    getRange and getVariance functions2

--- StatisticsCalculator/script.js
+++ StatisticsCalculator/script.js
@@ -26,6 +26,19 @@ const getMode = (array) => {
   return mode.join(", ");
 }
 
+const getRange = (array) => {
+  return Math.max(...array) - Math.min(...array);
+}
+
+const getVariance = (array) => {
+  const mean = getMean(array);
+  const variance = array.reduce((acc, el) => {
+    const difference = el - mean;
+    const squared = difference ** 2;
+    return acc + squared;
+  }, 0) / array.length;
+  return variance;
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
@@ -35,10 +48,14 @@ const calculate = () => {
   const mean = getMean(numbers);
   const median = getMedian(numbers);
   const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
 
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
   document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
 
 }
