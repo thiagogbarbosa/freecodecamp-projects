603be7d5a0edcb6b6ee2b12b077639db55bc89b0
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-21 17:34:16 +0100

    getMean Function

---
+++ StatisticsCalculator/script.js
@@ -0,0 +1,9 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const calculate = () => {
+    const value = document.querySelector("#numbers").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+    
+  
+  }
