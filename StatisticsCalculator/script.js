ec003eba79a30b1a93649480bf6700163a51c576
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-21 18:24:23 +0100

    getMode function

--- StatisticsCalculator/script.js
+++ StatisticsCalculator/script.js
@@ -9,15 +9,36 @@ const getMedian = (array) => {
   return median;
 }
 
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => {
+    counts[el] = (counts[el] || 0) + 1;
+  })
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort(
+    (a, b) => counts[b] - counts[a]
+  )[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(", ");
+}
+
+
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-
+  
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
-  document.querySelector("#median").textContent = median;
+
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
 
 }
