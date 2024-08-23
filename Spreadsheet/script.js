015a579ea6717b390cc11c15efec2875ffde98a8
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-23 08:45:47 +0100

    Creating input fields

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -11,6 +11,16 @@ window.onload = () => {
     container.appendChild(label);
   };
 
-    const letters = charRange("A", "J");
-    letters.forEach(createLabel);
-}
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach(number => {
+    createLabel(number);
+    letters.forEach(letter => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    })
+  })
+};
