6754cd1ac45728289cf952d37c60f733c24b71fa
thiagogbarbosa <tthiagogb@gmail.com>
2024-08-23 08:57:44 +0100

    Some built-in functions to the spreadSheet

--- Spreadsheet/script.js
+++ Spreadsheet/script.js
@@ -1,3 +1,9 @@
+const isEven = num => num % 2 === 0;
+const sum = nums => nums.reduce((acc, el) => acc + el, 0);
+const average = nums => sum(nums) / nums.length;
+
+//todo
+const median = (nums)=>{};
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
 const charRange = (start, end) => range(start.charCodeAt(0), end.charCodeAt(0)).map(code => String.fromCharCode(code));
